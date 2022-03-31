f63c9630d0ad49319c643931005c74e05cd7c519
wiwo-one <wojtekwieclawski@gmail.com>
2022-03-31 21:07:07 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-green-500">
+      <h1 className="text-7xl">SNAKE</h1>
+    </div>
+  );
+}
+
+export default App;
